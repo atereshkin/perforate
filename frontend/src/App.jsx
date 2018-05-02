829f1856e198bd662f1c5c70961eda7521a95c9b
Alex Tereshkin <atereshkin@y-node.com>
2018-05-02 18:24:05 +0300

    Minimal metric history chart with VX

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+
 import { AreaClosed } from '@vx/shape';
+import { Group } from '@vx/group';
+import { scaleTime, scaleLinear } from '@vx/scale';
+import { AxisLeft, AxisBottom } from '@vx/axis';
+import { LinearGradient } from '@vx/gradient';
+import { extent, max } from 'd3-array';
 
 class SimpleIndicator extends Component {
   render() {
@@ -40,20 +46,60 @@ class RequestsIndicator extends Component {
                                  }])}));
   }
   render() {
+    const width = 500;
+    const height = 200;
+    const margin = {
+      top: 10,
+      bottom: 40,
+      left: 40,
+      right: 10,
+    };
+    const xMax = width - margin.left - margin.right;
+    const yMax = height - margin.top - margin.bottom;
+    const x = d => d.timestamp; 
+    const y = d => d.value;
+    const xScale = scaleTime({
+      range: [0, xMax],
+      domain: extent(this.state.data, x)
+    });
+    const yScale = scaleLinear({
+      range: [yMax, 0],
+      domain: [0, max(this.state.data, y)],
+    });
     return (
-      <div>
-        <svg width={300} height={300}>
+      <div className="metric-with-history">
+        <svg width={width} height={height} className="history-chart">
           <Group top={margin.top} left={margin.left}>
+            <AxisLeft
+               scale={yScale}
+               top={0}
+               left={0}
+               stroke={'#1b1a1e'}
+               tickTextFill={'#1b1a1e'}
+               />
+            <AxisBottom
+               scale={xScale}
+               top={yMax}
+
+               stroke={'#1b1a1e'}
+               tickTextFill={'#1b1a1e'}
+               />
+            <LinearGradient
+               from='#fbc2eb'
+               to='#a6c1ee'
+               id='gradient'
+               />            
             <AreaClosed
                data={this.state.data}
                xScale={xScale}
                yScale={yScale}
                x={x}
                y={y}
-               fill={"red"}
-        />
-      </Group>
-    </svg>        
+               fill={"url(#gradient)"}
+               stroke={""}
+               />
+          </Group>
+        </svg>
         <SimpleIndicator title="Requests/second" value={this.state.value}/>
       </div>
     );
